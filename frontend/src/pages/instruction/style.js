cdeea28cc80bd83af88e3184c75d1eebc75d0616
Marcos Dias <marcos.costa@bcrcx.com>
2024-03-08 20:19:59 -0300

    Página instrução e inicio da API

---
+++ frontend/src/pages/instruction/style.js
@@ -0,0 +1,72 @@
+import { styled } from "styled-components";
+
+
+export const Body = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  margin-top: -0.5vh;
+  background-color: rgba(47, 151, 58, 0.2);
+`;
+
+export const Instruction = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: rgba(47, 151, 58, 1);
+  width: 100%;
+  padding: 3vh;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: white;
+  padding-bottom: 2vh;
+`;
+
+export const Subtitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: white;
+`;
+
+export const Back = styled.div`
+  display: flex;
+  position: absolute;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-start;
+  background-color: white;
+  margin-top: 8vh;
+  width: 80vw;
+  height: 80vh;
+  border-radius: 20px;
+`;
+
+export const Orientação = styled.div`
+  display: flex;
+  padding: 5vh;
+`;
+
+export const Instruction1 = styled.div`
+  display: flex;
+  margin-top: 2vh;
+  padding: 5vh;
+`;
+
+export const Instruction2 = styled.div`
+  display: flex;
+  padding: 5vh;
+`;
+
+export const Instruction3 = styled.div`
+  display: flex;
+  padding: 5vh;
+`;
--- frontend/src/pages/register/index.jsx
+++ frontend/src/pages/register/index.jsx
@@ -1,8 +1,9 @@
 import * as Css from "./style";
 import { FaKey } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
-import { MdEmail } from "react-icons/md";
 import logo from "../../img/logo.svg";
+import { MdEmail } from "react-icons/md";
+
 
 
 export default function Home() {
@@ -21,8 +22,16 @@ export default function Home() {
             <input type="email" placeholder="Email"></input>
         </Css.InputContainer>
         <Css.InputContainer>
+          <MdEmail />
+            <input type="email" placeholder="Confirmar email"></input>
+        </Css.InputContainer>
+        <Css.InputContainer>
             <FaKey />
             <input type="password" placeholder="Senha"></input>
+        </Css.InputContainer>
+        <Css.InputContainer>
+            <FaKey />
+            <input type="password" placeholder="Confirmar enha"></input>
         </Css.InputContainer>
           </Css.FormComponent>
         </Css.LoginContainer>
@@ -31,7 +40,7 @@ export default function Home() {
           <p>
             Não tem login?
             <b>
-            <Link to="/register"> Registre-se</Link>
+            <Link to="/Instruction"> Registre-se</Link>
             </b>
           </p>
         </Css.Login>
